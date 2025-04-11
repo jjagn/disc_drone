cb39db26935df2bfbcbe3f490583af3c2dffc006
jackson <jacksoncrawford2100@gmail.com>
2025-04-11 20:58:34 +1200

    not working not sure why

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
-#include "HardwareSerial.h"
-#include "esp32-hal-adc.h"
-#include <Arduino.h>
-#include <esp32-hal.h>
+#include "DShotRMT.h"
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <Arduino.h>
 #include <Wire.h>
 
 /// DSHOT
-#define POT_PIN 4      // potentiometer pin
+#define POT_PIN 23      // potentiometer pin
 #define ESC_PIN 16     // ESC control pin
 #define FWD_REV_PIN 17 // HIGH is forward, LOW is reverse
 #define MOTOR_POLES 14 // seems to be correct for my 2200KV motor
@@ -20,33 +18,19 @@
 // PID
 #define K_P 0
 #define K_I 0
-#define K_D 0.2 
-
-// DShot 600 has frame rate of 600 kHz, which is 133 ticks of a 80MHz clock
-#define SHORT_TICKS 44 // results in 555 nsec
-#define LONG_TICKS 89 // results in 1111 nsec; total is 133 ticks of 80MHz clock
-
-rmt_obj_t *rmt_send_g = NULL;
+#define K_D 0.2
 
 void mpuSetup(Adafruit_MPU6050 *mpu);
-void updateDShot(float motorVal);
-void dshotOutput(uint16_t value, bool telemetry, rmt_obj_t *rmt_send);
-void dshotSetup(rmt_obj_t **rmt_send);
 
-volatile uint16_t dshotUserInputValue = 0; // holds the pot reading for speed to be sent in packet
-volatile int interruptCounter = 0; // *1 counts the interrupts from timer. volitile to make it persistent
-int totalInterruptCounter; // *2
-hw_timer_t *timer = NULL;  // *4
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED; // *5 used to maintain sync between interupt
-                                  // and loop code
+// DSHOT
+const auto DSHOT_MODE = DSHOT300;
 
-void IRAM_ATTR onTimer() { // *7 ISR (interrupt service routine) for HW timer
-  portENTER_CRITICAL_ISR(&timerMux); // *8
-  interruptCounter++;                // *9
-  dshotOutput(dshotUserInputValue, false,
-              rmt_send_g); // send out the 16 bit digital command to ESC
-  portEXIT_CRITICAL_ISR(&timerMux); // *10
-} 
+// Define the failsafe and initial throttle values
+const auto FAILSAFE_THROTTLE = 999;
+const auto INITIAL_THROTTLE = 48;
+
+// Initialize a DShotRMT object for the motor
+DShotRMT motor01(ESC_PIN);
 
 // nav etc.
 #define GYRO_X_OFFSET -0.04
@@ -65,9 +49,9 @@ float delta_t, currentTime, previousTime;
 float gyroError = 0;
 int pot = 0;
 
-void setup() {          // *14
-  Serial.begin(115200); // *16
-  dshotSetup(&rmt_send_g);
+void setup() {
+  Serial.begin(115200);
+  motor01.begin(DSHOT_MODE, ENABLE_BIDIRECTION, 14);
   mpuSetup(&mpu);
 }
 
@@ -85,7 +69,7 @@ void loop() {
   roll += gyroY * delta_t;
   pitch += gyroX * delta_t;
   heading += gyroZ * delta_t;
-  
+
   // PID
   static float setpoint = 0;
   static float integrator = 0;
@@ -96,7 +80,7 @@ void loop() {
   // P
   output = error * K_P;
 
-  // I 
+  // I
   integrator += error * delta_t;
   output += integrator * K_I;
 
@@ -107,7 +91,8 @@ void loop() {
 
   // updateDShot(output);
   // updateDShot(1);
-
+  // Serial.println("updating dshot to motor");
+  motor01.send_dshot_value(INITIAL_THROTTLE);
 
   if (pacer++ > 200) {
     pacer = 0;
@@ -139,6 +124,8 @@ void loop() {
     Serial.print("Output: ");
     Serial.print(output);
     Serial.println("");
+
+    // send throttle to esc
   }
 }
 
@@ -155,80 +142,4 @@ void mpuSetup(Adafruit_MPU6050 *mpu) {
   mpu->setFilterBandwidth(MPU6050_BAND_260_HZ);
   Serial.println("");
   delay(100);
-}
-
-void updateDShot(float motorVal) {
-      // uint16_t dUR = analogRead(POT_PIN); // read the pot pin 0 - 4095
-      dshotUserInputValue = map(motorVal*1000, 0, 1540, 48, 1047); // 1 to 47 commands are reserved
-      // dshotUserInputValue = map(dUR, 0, 4096, 48, 1047); // 1 to 47 commands are reserved
-      if (dshotUserInputValue < 50 || dshotUserInputValue > 60000)
-        dshotUserInputValue = 0;
-      if (interruptCounter > 0) { // *27 true if it's time to enter interrupt code
-        portENTER_CRITICAL(&timerMux); // *29
-        interruptCounter--;            // *30
-        portEXIT_CRITICAL(&timerMux);  // *31
-        totalInterruptCounter++;       // *33
-        // Serial.printf("pot: %i   totalinterruptcounter: %i \n", dshotUserInputValue, totalInterruptCounter); // *35,36
-      }
-}
-
-void dshotSetup(rmt_obj_t **rmt_send, int outputPin) {
-  Serial.print("init starting\n");
-  pinMode(POT_PIN, INPUT);
-  pinMode(FWD_REV_PIN, INPUT_PULLUP);
-  pinMode(outputPin, OUTPUT);
-  *rmt_send = rmtInit(ESC_PIN, TX_NO_RX, RMT_MEM_64);
-  if (*rmt_send == NULL) {
-    Serial.println("init sender failed\n");
-  }
-  Serial.print("init sender success\n");
-  float realTick = rmtSetTick(*rmt_send, 12.5); // 12.5 nsec sample rate
-  Serial.printf("rmt_send tick set to: %fns\n", realTick);
-  while (millis() < 3500) {
-    dshotOutput(0, !TELEM_ON, *rmt_send);
-    delay(1);
-  }
-  timer = timerBegin(TMR_CHAN, BOARD_MHZ,
-                     COUNT_UP); // *18 returns pointer to timer structure
-  timerAttachInterrupt(timer, &onTimer,
-                       true); // *19 attatch timer interupt to onTimer routine
-                              // defined above: edge type=true
-  timerAlarmWrite(
-      timer, 1000,
-      true); // *20 1000 and prescale of 80 is a msec. reload the timeer=true
-  timerAlarmEnable(timer);
-}
-
-void dshotOutput(uint16_t value, bool telemetry,
-                 rmt_obj_t *rmt_send) { // creates packet to send
-  static rmt_data_t dshotPacket[16];    // structure defined in esp32_hal_h ?
-  uint16_t packet;
-  if (telemetry) {
-    packet = (value << 1) | 1;
-  } else {
-    packet = (value << 1) | 0;
-  }
-  int csum = 0; // holds checksum part of packet?
-  int csum_data = packet;
-  for (int i = 0; i < 3; i++) {
-    csum ^= csum_data;
-    csum_data >>= 4;
-  }
-  csum &= 0xf;
-  packet = (packet << 4) | csum;
-
-  for (int i = 0; i < 16; i++) {
-    dshotPacket[i].level0 = 1;
-    dshotPacket[i].level1 = 0;
-    if (packet & 0x8000) { // true for bit set
-      dshotPacket[i].duration0 = LONG_TICKS;
-      dshotPacket[i].duration1 = SHORT_TICKS;
-    } else { // true for bit clear
-      dshotPacket[i].duration0 = SHORT_TICKS;
-      dshotPacket[i].duration1 = LONG_TICKS;
-    }
-    packet <<= 1;
-  }
-  rmtWrite(rmt_send, dshotPacket, 16);
-  return;
 }
